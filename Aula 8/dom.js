bea589c25f1a6ec08de61eedb2459e2feaa8408d
Lucas Rosa <j36lucas@gmail.com>
2026-06-03 11:53:37 -0300

    dom

---
+++ Aula 8/dom.js
@@ -0,0 +1,9 @@
+let elemento = document.getElementById("titulo");
+elemento.style.color = "blue";
+elemento.style.fontSize = "24px";
+elemento.style.fontWeight = "bold";
+
+let paragrafo = document.getElementById("paragrafo");
+paragrafo.style.color = "green";
+paragrafo.style.fontSize = "18px";
+paragrafo.style.fontWeight = "normal";
